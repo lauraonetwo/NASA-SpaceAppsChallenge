a381dee7ffbf364e7d4664d4a81ec1ccd06a262f
Laura Wang Qiu <yiwang03@ucm.es>
2024-10-06 11:21:33 +0200

    Demolish and GraphDrawer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using TMPro;
+using System.Linq;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -6,6 +8,9 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance { get; private set; }
 
     public List<GameObject> objects = new List<GameObject>();
+
+    private GameObject budgetObj;
+    private ulong demolishPrice = 0;
 
     private void Awake()
     {
@@ -18,5 +23,28 @@ public class GameManager : MonoBehaviour
             Instance = this;
             DontDestroyOnLoad(gameObject);
         }
+
+        budgetObj = GameObject.Find("Budget");
+    }
+
+    public void Demolish()
+    {
+        foreach (GameObject obj in objects)
+        {
+            Info info = obj.GetComponent<Info>();
+            if (info.assignedEmpty != null)
+            {
+                info.assignedEmpty.GetComponent<Available>().isAvailable = true;
+            }
+            demolishPrice += info.price;
+            Destroy(obj);
+        }
+        objects.Clear();
+
+        TextMeshProUGUI text = budgetObj.GetComponent<TextMeshProUGUI>();
+        string numericString = new string(text.text.Where(char.IsDigit).ToArray());
+        ulong.TryParse(numericString, out ulong budget);
+        budget -= demolishPrice;
+        text.text = "Budget: $" + budget.ToString("N0");
     }
 }
---
+++ Assets/Scripts/GraphDrawer.cs
@@ -0,0 +1,178 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphDrawer : MonoBehaviour
+{
+    public List<List<float>> linesData = new List<List<float>>();
+    public List<Color> lineColors = new List<Color>();
+    public List<string> categories = new List<string> { "Public Transport", "Housing", "Green Spaces", "Waste Management", "Energy Production" };
+    public List<string> xAxisLabels = new List<string> { "Year 1", "Year 2", "Year 3", "Year 4", "Year 5" };
+
+    public float horizontalMargin = 50f;
+    public float verticalMargin = 50f;
+    public float lineThickness = 2f;
+    public float pointSize = 6f;
+    public float axesThickness = 2f;
+
+    public Vector2 legendPosition = new Vector2(10f, 50f);
+    public int legendFontSize = 14;
+
+    private void Start()
+    {
+        linesData.Add(new List<float> { 10, 20, 30, 40, 50 });
+        linesData.Add(new List<float> { 5, 35, 20, 45, 10 });
+        linesData.Add(new List<float> { 30, 25, 35, 15, 40 });
+        linesData.Add(new List<float> { 20, 15, 25, 35, 30 });
+        linesData.Add(new List<float> { 25, 10, 20, 30, 45 });
+
+        lineColors.Add(Color.blue);
+        lineColors.Add(Color.yellow);
+        lineColors.Add(Color.green);
+        lineColors.Add(Color.red);
+        lineColors.Add(Color.cyan);
+    }
+
+    private void OnGUI()
+    {
+        legendPosition.x = Mathf.Clamp(legendPosition.x, 0, Screen.width / 2 - 50);
+        DrawGraph();
+        DrawLegend();
+    }
+
+    private void DrawGraph()
+    {
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+        float graphWidth = screenWidth - 2 * horizontalMargin - 150;
+        float graphHeight = screenHeight - 2 * verticalMargin;
+        float startX = horizontalMargin + 150;
+        float startY = verticalMargin;
+
+        DrawAxesAndGrid(startX, startY, graphWidth, graphHeight);
+        float maxValue = GetMaxValue();
+
+        for (int lineIndex = 0; lineIndex < linesData.Count; lineIndex++)
+        {
+            List<float> lineData = linesData[lineIndex];
+
+            for (int i = 0; i < lineData.Count - 1; i++)
+            {
+                float normalizedValue1 = lineData[i] / maxValue;
+                float normalizedValue2 = lineData[i + 1] / maxValue;
+                float x1 = startX + (i * (graphWidth / (lineData.Count - 1)));
+                float y1 = startY + graphHeight - (normalizedValue1 * graphHeight);
+                float x2 = startX + ((i + 1) * (graphWidth / (lineData.Count - 1)));
+                float y2 = startY + graphHeight - (normalizedValue2 * graphHeight);
+
+                Drawing.DrawLine(new Vector2(x1, y1), new Vector2(x2, y2), lineColors[lineIndex], lineThickness);
+
+                Drawing.DrawCircle(new Vector2(x1, y1), pointSize, lineColors[lineIndex]);
+            }
+
+            float lastX = startX + ((lineData.Count - 1) * (graphWidth / (lineData.Count - 1)));
+            float lastY = startY + graphHeight - (lineData[lineData.Count - 1] / maxValue * graphHeight);
+            Drawing.DrawCircle(new Vector2(lastX, lastY), pointSize, lineColors[lineIndex]);
+        }
+
+        for (int i = 0; i < xAxisLabels.Count; i++)
+        {
+            float x = startX + (i * (graphWidth / (xAxisLabels.Count - 1)));
+            GUI.Label(new Rect(x - 20, startY + graphHeight + 10, 100, 20), xAxisLabels[i]);
+        }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            float percentage = (i * 20);
+            float y = startY + graphHeight - (i * (graphHeight / 5));
+            GUI.Label(new Rect(startX - 50, y - 10, 40, 20), percentage.ToString("0") + "%");
+        }
+    }
+
+    private void DrawLegend()
+    {
+        GUIStyle style = new GUIStyle();
+        style.fontSize = legendFontSize;
+        style.normal.textColor = Color.black;
+
+        for (int i = 0; i < categories.Count; i++)
+        {
+            style.normal.textColor = lineColors[i];
+            GUI.Label(new Rect(legendPosition.x, legendPosition.y + i * 50, 200, 20), categories[i], style);
+        }
+
+        GUI.color = Color.white;
+    }
+
+    private void DrawAxesAndGrid(float startX, float startY, float graphWidth, float graphHeight)
+    {
+        Drawing.DrawLine(new Vector2(startX, startY), new Vector2(startX, startY + graphHeight), Color.black, axesThickness);
+        Drawing.DrawLine(new Vector2(startX, startY + graphHeight), new Vector2(startX + graphWidth, startY + graphHeight), Color.black, axesThickness);
+
+        for (int i = 0; i < xAxisLabels.Count; i++)
+        {
+            float x = startX + (i * (graphWidth / (xAxisLabels.Count - 1)));
+            Drawing.DrawLine(new Vector2(x, startY), new Vector2(x, startY + graphHeight), Color.gray, 1f);
+        }
+
+        for (int i = 0; i <= 5; i++)
+        {
+            float y = startY + graphHeight - (i * (graphHeight / 5));
+            Drawing.DrawLine(new Vector2(startX, y), new Vector2(startX + graphWidth, y), Color.gray, 1f);
+        }
+    }
+
+    private float GetMaxValue()
+    {
+        float max = float.MinValue;
+        foreach (var line in linesData)
+        {
+            foreach (var value in line)
+            {
+                if (value > max)
+                    max = value;
+            }
+        }
+        return max;
+    }
+}
+
+public static class Drawing
+{
+    private static Texture2D lineTex = null;
+
+    public static void DrawLine(Vector2 pointA, Vector2 pointB, Color color, float width)
+    {
+        if (!lineTex)
+        {
+            lineTex = new Texture2D(1, 1);
+        }
+
+        Color previousColor = GUI.color;
+        GUI.color = color;
+
+        Matrix4x4 matrix = GUI.matrix;
+        float angle = Mathf.Atan2(pointB.y - pointA.y, pointB.x - pointA.x) * 180f / Mathf.PI;
+        float length = (pointB - pointA).magnitude;
+
+        GUIUtility.RotateAroundPivot(angle, pointA);
+        GUI.DrawTexture(new Rect(pointA.x, pointA.y, length, width), lineTex);
+        GUI.matrix = matrix;
+
+        GUI.color = previousColor;
+    }
+
+    public static void DrawCircle(Vector2 center, float radius, Color color)
+    {
+        if (!lineTex)
+        {
+            lineTex = new Texture2D(1, 1);
+        }
+
+        Color previousColor = GUI.color;
+        GUI.color = color;
+
+        GUI.DrawTexture(new Rect(center.x - radius / 2, center.y - radius / 2, radius, radius), lineTex);
+
+        GUI.color = previousColor;
+    }
+}
